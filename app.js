65fe487885a7181fee835e511a6c3d80006f6e07
Yashwant Kargwal <154042689+yashwant-kargwal@users.noreply.github.com>
2025-02-10 12:08:31 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+let selectForm = document.querySelector("#from");
+
+const upgradeFlag = (element) =>{
+    const currentFlag = element.value;
+    console.log(currentFlag);
+}
+console.log(upgradeFlag);
